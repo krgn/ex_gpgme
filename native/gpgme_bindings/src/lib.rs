6f0ac017adef8e02120da3469fd46847ca94c4f5
Karsten Gebbert <k@ioctl.it>
2019-03-01 23:42:31 +0100

    [rust] return a list of key maps for list_keys

--- native/gpgme_bindings/src/lib.rs
+++ native/gpgme_bindings/src/lib.rs
@@ -31,34 +31,48 @@ fn list_keys<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     mode.insert(KeyListMode::LOCAL);
 
     let mut ctx = Context::from_protocol(proto).expect("context");
+    let mut key_list = vec![];
 
     ctx.set_key_list_mode(mode).expect("key list mode");
     let mut keys = ctx.keys().expect("keys");
     for key in keys.by_ref().filter_map(|x| x.ok()) {
-        println!("keyid   : {}", key.id().unwrap_or("?"));
-        println!("fpr     : {}", key.fingerprint().unwrap_or("?"));
-        println!(
-            "caps    : {}{}{}{}",
-            if key.can_encrypt() { "e" } else { "" },
-            if key.can_sign() { "s" } else { "" },
-            if key.can_certify() { "c" } else { "" },
-            if key.can_authenticate() { "a" } else { "" }
-        );
-        println!(
-            "flags   :{}{}{}{}{}{}",
-            if key.has_secret() { " secret" } else { "" },
-            if key.is_revoked() { " revoked" } else { "" },
-            if key.is_expired() { " expired" } else { "" },
-            if key.is_disabled() { " disabled" } else { "" },
-            if key.is_invalid() { " invalid" } else { "" },
-            if key.is_qualified() { " qualified" } else { "" }
-        );
+        let mut map = Term::map_new(env);
+
+        map = map.map_put("keyid".encode(env), key.id().unwrap_or("?").encode(env))?;
+        map = map.map_put(
+            "fpr".encode(env),
+            key.fingerprint().unwrap_or("?").encode(env),
+        )?;
+        map = map.map_put("can_encrypt".encode(env), key.can_encrypt().encode(env))?;
+        map = map.map_put("can_sign".encode(env), key.can_sign().encode(env))?;
+        map = map.map_put("can_certify".encode(env), key.can_certify().encode(env))?;
+        map = map.map_put(
+            "can_authenticate".encode(env),
+            key.can_authenticate().encode(env),
+        )?;
+        map = map.map_put("has_secret".encode(env), key.has_secret().encode(env))?;
+        map = map.map_put("is_revoked".encode(env), key.is_revoked().encode(env))?;
+        map = map.map_put("is_expired".encode(env), key.is_expired().encode(env))?;
+        map = map.map_put("is_disabled".encode(env), key.is_disabled().encode(env))?;
+        map = map.map_put("is_invalid".encode(env), key.is_invalid().encode(env))?;
+        map = map.map_put("is_qualified".encode(env), key.is_qualified().encode(env))?;
+
+        let mut users = vec![];
+
         for (i, user) in key.user_ids().enumerate() {
-            println!("userid {}: {}", i, user.id().unwrap_or("[none]"));
-            println!("valid  {}: {:?}", i, user.validity())
+            let mut user_map = Term::map_new(env);
+            user_map = user_map.map_put(
+                "userid".encode(env),
+                user.id().unwrap_or("[none]").encode(env),
+            )?;
+            user_map =
+                user_map.map_put("valid".encode(env), user.validity().to_string().encode(env))?;
+            users.push(user_map);
         }
-        println!("");
+        map = map.map_put("users".encode(env), users.encode(env))?;
+
+        key_list.push(map);
     }
 
-    Ok(atoms::ok().encode(env))
+    Ok((atoms::ok(), key_list).encode(env))
 }
